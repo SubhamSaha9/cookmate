31b3f26eb73b52437ff20f27ce08f1ddaa3d9d7c
SubhamSaha9 <sahasubham7478@gmail.com>
2025-03-26 02:02:29 +0530

    backend created

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const cors = require("cors");
+const { connect } = require("./config/database");
+const { cloudinaryConnect } = require("./config/cloudinary");
+
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+        credentials: true,
+    })
+);
+
+connect();
+cloudinaryConnect();
+
+app.get("/", (req, res) => {
+    return res.json({
+        success: true,
+        message: "Your server is up and running ...",
+    });
+})
+
+app.listen(port, () => {
+    console.log(`app is listening to ${port}`);
+})
