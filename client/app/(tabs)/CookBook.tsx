fd725ea02d648cfa29d2cfb62ad6f46d5192cd80
SubhamSaha9 <sahasubham7478@gmail.com>
2025-04-12 02:41:52 +0530

    add ui for save recipe

--- client/app/(tabs)/CookBook.tsx
+++ client/app/(tabs)/CookBook.tsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, Alert, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { COLORS } from "@/styles/Colors";
 import { useSelector } from "react-redux";
@@ -7,6 +14,7 @@ import { Recipe } from "../recipe-by-category";
 import axios from "axios";
 import RecipeCard from "@/components/common/RecipeCard";
 import Loading from "@/components/ui/Loading";
+import { Ionicons } from "@expo/vector-icons";
 
 const BASE_URI = process.env.EXPO_PUBLIC_API_URL;
 
@@ -14,13 +22,19 @@ export default function CookBook() {
   const { token } = useSelector((state: RootState) => state.auth);
   const [loader, setLoader] = useState<boolean>(false);
   const [recipeList, setRecipeList] = useState<Recipe[]>([]);
+  const [activeTab, setActiveTab] = useState<number>(1);
 
-  const getRecipesByUser = async () => {
+  const getRecipesByUserOrSaved = async () => {
     setLoader(true);
     try {
-      const { data } = await axios.get(`${BASE_URI}/recipe/user-recipe`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const { data } = await axios.get(
+        `${BASE_URI}/recipe/${
+          activeTab === 1 ? "user-recipe" : "saved-recipe"
+        }`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       setLoader(false);
       if (!data.success) {
         Alert.alert("Error", data.message);
@@ -36,11 +50,28 @@ export default function CookBook() {
   };
 
   useEffect(() => {
-    token && getRecipesByUser();
-  }, []);
+    token && getRecipesByUserOrSaved();
+  }, [activeTab]);
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>CookBook</Text>
+
+      <View style={styles.tab}>
+        <TouchableOpacity
+          style={[styles.tabContainer, { opacity: activeTab === 1 ? 1 : 0.4 }]}
+          onPress={() => setActiveTab(1)}
+        >
+          <Ionicons name="sparkles-sharp" size={24} color={"black"} />
+          <Text style={styles.tabText}>My Recipe</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tabContainer, { opacity: activeTab === 2 ? 1 : 0.4 }]}
+          onPress={() => setActiveTab(2)}
+        >
+          <Ionicons name="bookmark" size={24} color={"black"} />
+          <Text style={styles.tabText}>Saved</Text>
+        </TouchableOpacity>
+      </View>
 
       <Loading visible={loader} />
       <FlatList
@@ -48,7 +79,7 @@ export default function CookBook() {
         keyExtractor={(item) => item._id}
         numColumns={2}
         refreshing={loader}
-        onRefresh={getRecipesByUser}
+        onRefresh={getRecipesByUserOrSaved}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <View
@@ -73,5 +104,22 @@ const styles = StyleSheet.create({
   heading: {
     fontFamily: "outfit-bold",
     fontSize: 25,
+  },
+  tab: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 6,
+    marginBottom: 6,
+  },
+  tabContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 5,
+  },
+  tabText: {
+    fontFamily: "outfit",
+    fontSize: 20,
   },
 });
